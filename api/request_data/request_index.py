42b4f1a661f91b31493b3138377956b097046ed9
郑逸润 <zhengyirun@dianchu.com>
2022-03-05 16:36:06 +0800

    feat:init

--- api/request_data/request_index.py
+++
@@ -1,6 +0,0 @@
-# 请求字段封装索引
-class RequestIndex(object):
-    user = "user"
-    Password = "password"
-    device_id = "device_id"
-    device_type = "device_type"
--- api/response_data/response_index.py
+++
@@ -1,7 +0,0 @@
-# 响应字段参数封装索引
-
-
-class RegRespDataIndex(object):
-    code = "code"
-    info = "info"
-    data = "data"
